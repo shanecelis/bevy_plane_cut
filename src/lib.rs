22f9821ed8e503d68d90a88808efdf414bc4692e
Shane Celis <shane.celis@gmail.com>
2024-07-05 04:41:01 -0400

    chore: Update to bevy 0.14.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bevy_plane_cut/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/bevy_plane_cut/0.2.0")]
 #![doc = include_str!("../README.md")]
 #![forbid(missing_docs)]
 
